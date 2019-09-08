f9a109de045e02904a8d6c5be75d713171f54c67
yasuyuky <yasuyuki.ymd@gmail.com>
2019-09-08 21:27:15 +0900

    Add built-in test

--- src/main.rs
+++ src/main.rs
@@ -170,3 +170,34 @@ fn main() -> io::Result<()> {
     println!("{}", compile_buffer(&buffer));
     Ok(())
 }
+
+#[allow(unused_imports)]
+mod test {
+    use crate::compile_buffer;
+    use std::io::Write;
+    use std::process::{Command, Stdio};
+
+    #[test]
+    fn test_line_with_status_code() {
+        let test_sets = vec![
+            ("0", 0),
+            ("1 + 1", 2),
+            ("1 * 2 + 3", 1 * 2 + 3),
+            ("1 + 2*3", 1 + 2 * 3),
+            ("1*2 + 3*4", 1 * 2 + 3 * 4),
+        ];
+        for (input, result) in test_sets {
+            let ir = compile_buffer(&input);
+            let mut clang = Command::new("clang")
+                .args(&["-Wno-override-module", "-o", "tmp", "-x", "ir", "-"])
+                .stdin(Stdio::piped())
+                .spawn()
+                .expect("compile ir");
+            let cstdin = clang.stdin.as_mut().expect("open stdin");
+            cstdin.write_all(ir.as_bytes()).expect("write to stdin");
+            clang.wait().expect("exit clang compile");
+            let status = Command::new("./tmp").status().expect("execute bin");
+            assert_eq!(status.code(), Some(result));
+        }
+    }
+}
