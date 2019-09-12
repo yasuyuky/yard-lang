a023f68a6de1e0da1826dba589ca62ce38c4f6b2
yasuyuky <yasuyuki.ymd@gmail.com>
2019-09-12 23:55:59 +0900

    Update token.rs

--- src/token.rs
+++ src/token.rs
@@ -1,26 +1,59 @@
+use std::iter::FromIterator;
+
 pub enum Token {
     Number(String),
     Arithmetic(String),
 }
 
-pub fn tokenize(buf: &str) -> Vec<Token> {
-    let mut res: Vec<Token> = vec![];
+#[derive(Debug, PartialEq, PartialOrd, Clone, Copy)]
+enum CharType {
+    WhiteSpace,
+    Digit,
+    Alphabetic,
+    Punctuation,
+    Unknown,
+}
+
+impl From<&char> for CharType {
+    fn from(c: &char) -> Self {
+        if c.is_ascii_whitespace() {
+            Self::WhiteSpace
+        } else if c.is_ascii_digit() {
+            Self::Digit
+        } else if c.is_ascii_alphabetic() {
+            Self::Alphabetic
+        } else if c.is_ascii_punctuation() {
+            Self::Punctuation
+        } else {
+            Self::Unknown
+        }
+    }
+}
+
+fn split_to_raw_tokens(buf: &str) -> Vec<(CharType, Vec<char>)> {
+    let mut ret: Vec<(CharType, Vec<char>)> = vec![];
     for c in buf.chars() {
-        if c.is_whitespace() {
-            continue;
-        } else if c.is_numeric() {
-            let last = res.pop().unwrap_or(Token::Number(String::new()));
-            match last {
-                Token::Number(s) => {
-                    res.push(Token::Number(s + c.to_string().as_str()));
-                }
-                Token::Arithmetic(s) => {
-                    res.push(Token::Arithmetic(s));
-                    res.push(Token::Number(c.to_string()));
+        match ret.last_mut() {
+            None => ret.push((CharType::from(&c), vec![c])),
+            Some((ty, v)) => {
+                if *ty == CharType::from(&c) {
+                    v.push(c)
+                } else {
+                    ret.push((CharType::from(&c), vec![c]))
                 }
             }
-        } else {
-            res.push(Token::Arithmetic(c.to_string()));
+        };
+    }
+    return ret;
+}
+
+pub fn tokenize(buf: &str) -> Vec<Token> {
+    let mut res: Vec<Token> = vec![];
+    for (ty, v) in split_to_raw_tokens(buf) {
+        match ty {
+            CharType::Digit => res.push(Token::Number(String::from_iter(v.into_iter()))),
+            CharType::Punctuation => res.push(Token::Arithmetic(String::from_iter(v.into_iter()))),
+            _ => continue,
         }
     }
     res
