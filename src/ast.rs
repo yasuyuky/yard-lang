f64bb9e2b20e3aa5fa3ba68fb8b00cf87448271c
yasuyuky <yasuyuki.ymd@gmail.com>
2019-09-16 17:29:13 +0900

    Siimplify

--- src/ast.rs
+++ src/ast.rs
@@ -10,28 +10,26 @@ pub fn make_ast(mut tokens: Vec<Token>) -> Ast {
     let mut stack: Vec<Exp> = vec![Exp::Undef];
     for t in tokens.iter_mut() {
         eprintln!("{:?}", stack);
-        match stack.pop() {
-            Some(ref mut exp) => match t {
-                Token::Operator(s) => match s.as_str() {
-                    "+" => stack.push(exp.make_bexpl(BinOp::PlusMinus(Additive::Plus))),
-                    "-" => stack.push(exp.make_bexpl(BinOp::PlusMinus(Additive::Minus))),
-                    "*" => stack.push(exp.make_bexpl(BinOp::MulDiv(Multitive::Mul))),
-                    "/" => stack.push(exp.make_bexpl(BinOp::MulDiv(Multitive::Div))),
-                    "=" => {
-                        if let Exp::Ident(s) = exp {
-                            stack.push(make_assign(&s))
-                        }
+        let mut exp = stack.pop().unwrap_or(Exp::Undef);
+        match t {
+            Token::Operator(s) => match s.as_str() {
+                "+" => stack.push(exp.make_bexpl(BinOp::PlusMinus(Additive::Plus))),
+                "-" => stack.push(exp.make_bexpl(BinOp::PlusMinus(Additive::Minus))),
+                "*" => stack.push(exp.make_bexpl(BinOp::MulDiv(Multitive::Mul))),
+                "/" => stack.push(exp.make_bexpl(BinOp::MulDiv(Multitive::Div))),
+                "=" => {
+                    if let Exp::Ident(s) = exp {
+                        stack.push(make_assign(&s))
                     }
-                    ";" => {
-                        stack.push(exp.clone());
-                        stack.push(Exp::Undef)
-                    }
-                    _ => panic!("Undefined arithmetic operator"),
-                },
-                Token::Number(s) => stack.push(exp.extend(Exp::Num(s.to_string()))),
-                Token::Ident(s) => stack.push(exp.extend(Exp::Ident(s.to_string()))),
+                }
+                ";" => {
+                    stack.push(exp.clone());
+                    stack.push(Exp::Undef)
+                }
+                _ => panic!("Undefined arithmetic operator"),
             },
-            None => unreachable!(),
+            Token::Number(s) => stack.push(exp.extend(Exp::Num(s.to_string()))),
+            Token::Ident(s) => stack.push(exp.extend(Exp::Ident(s.to_string()))),
         }
     }
     Ast::Stmt(stack)
